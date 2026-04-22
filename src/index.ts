c484ad0701e110789e56515b917229bb1130aebd
Amit Kumar <amitkrmaurya09@gmail.com>
2026-04-22 16:02:24 +0530

    basic

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
+const drink = "chai"
 
-function greet(person: string): string{
-    return `hello ${person}`
-}
+const drink2: string = "chai"
+
+const cups = 2;
 
 
-console.log(greet("genius"))
+const cups2: number = 2
+
+const isChai = true;
+
+
+const isChai2: boolean = false;
 
